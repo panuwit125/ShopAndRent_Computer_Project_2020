3c519cf9084f8a712de065cbf0979e51307aea98
panuwit125 <58617796+panuwit125@users.noreply.github.com>
2020-09-20 23:30:13 +0700

    Update

--- pages/_app.js
+++ pages/_app.js
@@ -4,19 +4,9 @@ import { Provider } from "react-redux";
 import { createWrapper } from "next-redux-wrapper";
 import store from "../store/store";
 import Router from "next/router";
-import * as gtag from "../lib/gtag";
 import "../styles/global.scss";
 
 const MyApp = ({ Component, pageProps }) => {
-  /*useEffect(() => {
-    const handleRouteChange = (url) => {
-      gtag.pageview(url);
-    };
-    Router.events.on("routeChangeComplete", handleRouteChange);
-    return () => {
-      Router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, []);*/
 
   return (
     <Provider store={store}>
